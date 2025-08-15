9d31abf795c414ec125d43bd54740daec4842cfd
Joel Miller <99599435+Jaymi-01@users.noreply.github.com>
2025-08-15 15:42:01 +0100

    Add proficiency constants for various technologies

---
+++ src/constant/Index.jsx
@@ -0,0 +1,34 @@
+import { FaArrowRight } from "react-icons/fa";
+
+export const proficiency = [
+    {
+        text: "Html"
+    },
+    {
+        text: "CSS"
+    },
+    {
+        text: "Javascript"
+    },
+    {
+        text: "Bootstrap"
+    },
+    {
+        text: "Tailwind"
+    },
+    {
+        text: "React.JS"
+    },
+    {
+        text: "Next.JS"
+    },
+    {
+        text: "React Native"
+    },
+    {
+        text: "C#"
+    },
+    {
+        text: "Firebase"
+    },
+];
